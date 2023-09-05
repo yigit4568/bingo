10421c0449076574af6fbb2810fa68aca3e6afa5
yigit4568 <83416665+yigit4568@users.noreply.github.com>
2023-09-05 18:17:06 +0300

    Update worker.js

--- cloudflare/worker.js
+++ cloudflare/worker.js
@@ -1,4 +1,4 @@
-const TRAGET_HOST='hf4all-bingo.hf.space' // 请将此域名改成你自己的，域名信息在设置》站点域名查看。
+const TRAGET_HOST='yigithan4568-cordelius.hf.space' // 请将此域名改成你自己的，域名信息在设置》站点域名查看。
 
 export default {
   async fetch(request) {
